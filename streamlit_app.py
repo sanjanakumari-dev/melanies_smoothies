caac340670b419ecf8e25514349204fa0086b8ba
sanjanakumari-dev <varshasingh98888@gmail.com>
2024-10-05 15:46:27 +0530

    adding fruit column

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,12 @@ if ingredients_list:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+if ingredients_list:
+         ingredients_string = ''
+
+        for fruit_chosen in ingredients_list:
+                  ingredients_string +=fruit_chosen + ' '
+                   st.subheader(fruit_choosen + 'Nutrition Information')
+                   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
+                   fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
