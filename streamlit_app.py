00ca41a780aefe4393b15dd011ce4b7e5764b9b3
sanjanakumari-dev <varshasingh98888@gmail.com>
2024-10-05 15:56:00 +0530

    adding fruitchosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,16 +30,12 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+          ingredients_string = ''
 
-
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
+          for fruit_chosen in ingredients_list:
                   ingredients_string +=fruit_chosen + ' '
-                  st.subheader(fruit_choosen + 'Nutrition Information')
-                  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
+                  st.subheader(fruit_chosen + 'Nutrition Information')
+                  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
                   fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
