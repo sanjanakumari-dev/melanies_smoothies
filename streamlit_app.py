b727ea5944fb95ab406a9f8bb08a64c2a0fe7e92
sanjanakumari-dev <varshasingh98888@gmail.com>
2024-10-05 15:22:18 +0530

    removed get active session

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write(' The name on your smoothie will be:', name_on_order)
 
 
 
-session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
