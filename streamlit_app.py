37866e7eb3c38babb97049929154bd0457284541
sanjanakumari-dev <varshasingh98888@gmail.com>
2024-10-05 16:28:01 +0530

    updating fruit column

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if ingredients_list:
     
      ingredients_string = ''
 
-          for fruit_choosen in ingredients_list:
+          for fruit_chosen in ingredients_list:
               ingredients_string += fruit_chosen + ' '
 
               search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
